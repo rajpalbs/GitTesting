de16c2d132c9825742d869a9d8cd9a0248f440e2
Rajpalsinh Songara <rajpalbs@gmail.com>
2017-04-19 23:31:39 +0530

    Second Draft

---
+++ src/com/hamming/utility/General.java
@@ -0,0 +1,12 @@
+package com.hamming.utility;
+
+import java.util.UUID;
+
+/**
+ * Created by rajpalsinhsongara on 19/4/17.
+ */
+public class General {
+    public static String uuidGenerator(){
+        return UUID.randomUUID().toString();
+    }
+}
