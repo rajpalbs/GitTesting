30ed3eeff53f3f156473afd8710ab8b543e1da07
Rajpalsinh Songara <rajpalbs@gmail.com>
2017-04-22 15:32:12 +0530

    Adding test-cases

--- src/test/java/com/hamming/test/StringUtilTest.java
+++ src/test/java/com/hamming/test/StringUtilTest.java
@@ -10,8 +10,35 @@ import static org.junit.Assert.*;
 public class StringUtilTest {
     @Test
     public void base64EncodeDecodeTest(){
-        String inputString = "Rajpalsinh Songara";
+        String inputString="Rajpalsinh Songara";
         String resultString = StringUtil.decode(StringUtil.encode(inputString));
         assertEquals(inputString,resultString);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void base64EncodeFail(){
+        StringUtil.encode(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void base64DecodeFail(){
+        StringUtil.decode_urlsafe(null);
+    }
+
+    @Test
+    public void base64EncodeDecodeURLSafeTest(){
+        String inputString="Rajpalsinh Songara";
+        String resultString = StringUtil.decode(StringUtil.encode_urlsafe(inputString));
+        assertEquals(inputString,resultString);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void base64EncodeURLSafeFail(){
+        StringUtil.encode_urlsafe(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void base64DecodeURLSafeFail(){
+        StringUtil.decode_urlsafe(null);
     }
 }
